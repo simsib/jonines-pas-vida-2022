6b0faceec7ce6f095ccdf643d8c70747d8c4e2ec
weedma <vydmantas.kolduba@gmail.com>
2022-06-19 21:05:23 +0300

    remove dist from git ignore, add dist folder

---
+++ index.672d4772.js
@@ -0,0 +1,40 @@
+function clean0(timeto0) {
+    if (timeto0 < 10) var timeto0 = "0" + timeto0;
+    else var timeto0 = timeto0;
+    return timeto0;
+}
+function showCountdown() {
+    const countDownDate = new Date("2022-06-23T19:00").getTime();
+    document.querySelector(".time-content").classList.add("timer-ready");
+    counter(countDownDate, undefined);
+    const x = setInterval(function() {
+        counter(countDownDate, x);
+    }, 1000);
+}
+function counter(countDownDate, x) {
+    var now = new Date().getTime();
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / 86400000);
+    var hours = Math.floor(distance % 86400000 / 3600000);
+    var minutes = Math.floor(distance % 3600000 / 60000);
+    var seconds = Math.floor(distance % 60000 / 1000);
+    var days = clean0(days);
+    var hours = clean0(hours);
+    var minutes = clean0(minutes);
+    var seconds = clean0(seconds);
+    // Output the result in an element with id="demo"
+    document.getElementById("Days").innerHTML = days;
+    document.getElementById("Hours").innerHTML = hours;
+    document.getElementById("Minutes").innerHTML = minutes;
+    document.getElementById("Seconds").innerHTML = seconds;
+    // If the count down is over, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}
+showCountdown();
+
+//# sourceMappingURL=index.672d4772.js.map
